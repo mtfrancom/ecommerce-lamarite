978636f505fd5f2ce54d91f6adbc9c373a747d66
Maria Franco <mariamizzoni10@gmail.com>
2022-01-30 17:41:33 -0300

    item-counter

--- Desktop/reactjs/ecommerce/src/components/Item/Item.js
+++ Desktop/reactjs/ecommerce/src/components/Item/Item.js
@@ -1,13 +1,23 @@
+import { useState } from "react";
+import ItemCounter from "../item-counter/ItemCounter"
 
-const Item = ({nombre, precio, id, setSelectedItem}) => {
-    const selecItem = () => setSelectedItem({nombre, precio, id});
+
+const Item = ({nombre, precio, id, setSelectedItem, stock, description}) => {
+
+    const [stockSelected, setStockSelected] = useState(0);
+    const selecItem = () => setSelectedItem({nombre, precio, id, stock:stockSelected, description });
 
   return (
   <div>
       <h2>Nombre del producto: {nombre} </h2>
       <h2>Precio del producto: {precio}</h2>
+      <h2>Descripcion: {description} </h2>
+      <h2>Stock: {stock} </h2>
+      <ItemCounter stock={stock} setStockSelected={setSelectedItem} />
       <button onClick={selecItem}>Seleccionar Producto </button>
+      <hr/>
   </div>
+
   );
 };
 
---
+++ Desktop/reactjs/ecommerce/src/components/item-counter/ItemCounter.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState} from 'react';
+
+const ItemCounter = ( { setStockSelected, stock } ) => {
+
+    const [counter, setCounter] = useState(0);
+
+    useEffect (() => {
+      setStockSelected(counter);
+    }, [counter])
+   
+    const menosCounter = () => {
+        if (counter <= 0) return;
+        setCounter(counter -1);
+    };
+
+    const masCounter = () => {
+        if (counter >= stock) return;
+        setCounter(counter +1);
+    }
+  return (
+    <div>
+    <button onClick={menosCounter}>-</button>
+    <span>{counter}</span>
+    <button onClick={masCounter}>+</button>
+    </div>
+
+  ); 
+};
+
+export default ItemCounter;
